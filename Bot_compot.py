6ef9f354e6f839e94a5e18ecc225c906cd4d1145
cheRut <diemason@list.ru>
2020-01-01 01:52:48 +0300

    update compot

--- Bot_compot.py
+++ Bot_compot.py
@@ -1,6 +1,6 @@
 
 import time
-from multiprocessing.context import Process
+from multiprocessing import Process
 
 import telebot
 from BetOddClass import BetODD
@@ -22,8 +22,7 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['run'])
 def send_data(message):
-    live_data.main()
-    bet_odd.init_betodds()
+
     s = bet_odd.t
     if len(s)==0:
         msg = bot.send_message(message.chat.id, "there  are not to show ")
@@ -34,12 +33,12 @@ def send_data(message):
 
 def  check_send_message():
     live_data = LiveData()
-    betOdd = BetODD()
+
     while True:
         try:
             live_data.main()
-            betOdd.create_line_table()
-            betOdd.select_by_waiting()
+            bet_odd.create_line_table()
+            bet_odd.select_by_waiting()
             # with open('mb.log', 'w'):
             #     logging.basicConfig(filename="mb.log", level=logging.INFO)
             time.sleep(30)
