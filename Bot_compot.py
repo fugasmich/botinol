5e357adbce45eda4ede4e2792944af40496f0867
cheRut <diemason@list.ru>
2020-01-01 01:25:45 +0300

    bot update rus translate

--- Bot_compot.py
+++ Bot_compot.py
@@ -18,7 +18,7 @@ bet_odd = BetODD()
 
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    msg = bot.send_message(message.chat.id, 'Привет! Я codex_bot!')
+    msg = bot.send_message(message.chat.id, 'HELLO')
 
 @bot.message_handler(commands=['run'])
 def send_data(message):
@@ -26,7 +26,7 @@ def send_data(message):
     bet_odd.init_betodds()
     s = bet_odd.t
     if len(s)==0:
-        msg = bot.send_message(message.chat.id, "пока нехуй ловить")
+        msg = bot.send_message(message.chat.id, "there  are not to show ")
     else:
         for i in range(0, len(s)):
             msg = bot.send_message(message.chat.id, s[i])
