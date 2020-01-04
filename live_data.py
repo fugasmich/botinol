15fffd04cb3816f3008abe8866a25c68b1629178
cheRut <diemason@list.ru>
2020-01-04 14:10:35 +0300

    add chromedriver

--- live_data.py
+++ live_data.py
@@ -99,7 +99,7 @@ class LiveData():
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
-        self.driver = webdriver.Chrome(executable_path=os.environ.get("chromedriver"), chrome_options=chrome_options)
+        self.driver = webdriver.Chrome()
 
 
         web_r = requests.get(ENUMS.URL_MAIN.value)
